ad82169a578c3cdcc35d97b33b41807dbe4cd9ef
Facundoreboredo <reboredofacundo5@gmail.com>
2025-09-13 20:44:37 -0300

    Add files via upload

---
+++ TP Patrones Creacionales/src/Main/factory/LogisticaLibro.java
@@ -0,0 +1,19 @@
+package Main.factory;
+import lombok.*;
+@Builder
+
+public class LogisticaLibro {
+    public static Libro crearLibro(String tipo, String Titulo, String Autor) {
+        switch (tipo){
+            case "Digital": return LibroDigital.builder()
+                    .Titulo(Titulo)
+                    .Autor(Autor)
+                    .build();
+            case "Fisico": return LibroFisico.builder()
+                    .Titulo(Titulo)
+                    .Autor(Autor)
+                    .build();
+            default: throw new IllegalArgumentException("Tipo desconocido");
+        }
+    }
+}
